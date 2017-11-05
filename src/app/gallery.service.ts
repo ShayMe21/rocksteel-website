a38d028cc5aa4d10670336d4948259b37256e50a
Shayan Memari <memshayan@gmail.com>
2017-11-06 09:36:14 +1100

    Fixed images IDs.

--- src/app/gallery.service.ts
+++ src/app/gallery.service.ts
@@ -20,11 +20,11 @@ const IMAGES = [
   { "id": 3, "title": "Third", "url": "../assets/images/3-Thumb.jpg", "category": "commercial" },
   { "id": 4, "title": "Fourth", "url": "../assets/images/4-Thumb.jpg", "category": "commercial" },
   { "id": 5, "title": "Fifth", "url": "../assets/images/5-Thumb.jpg", "category": "residential" },
-  { "id": 5, "title": "Sixth", "url": "../assets/images/6-Thumb.jpg", "category": "residential" },
-  { "id": 5, "title": "Seventh", "url": "../assets/images/7-Thumb.jpg", "category": "commercial" },
-  { "id": 5, "title": "Eight", "url": "../assets/images/8-Thumb.jpg", "category": "commercial" },
-  { "id": 5, "title": "Ninth", "url": "../assets/images/9-Thumb.jpg", "category": "others" },
-  { "id": 5, "title": "Tenth", "url": "../assets/images/10-Thumb.jpg", "category": "others" },
-  { "id": 5, "title": "Eleventh", "url": "../assets/images/11-Thumb.jpg", "category": "others" }
+  { "id": 6, "title": "Sixth", "url": "../assets/images/6-Thumb.jpg", "category": "residential" },
+  { "id": 7, "title": "Seventh", "url": "../assets/images/7-Thumb.jpg", "category": "commercial" },
+  { "id": 8, "title": "Eight", "url": "../assets/images/8-Thumb.jpg", "category": "commercial" },
+  { "id": 9, "title": "Ninth", "url": "../assets/images/9-Thumb.jpg", "category": "others" },
+  { "id": 10, "title": "Tenth", "url": "../assets/images/10-Thumb.jpg", "category": "others" },
+  { "id": 11, "title": "Eleventh", "url": "../assets/images/11-Thumb.jpg", "category": "others" }
   //0415526726
 ]
